2cc231bf947bca87af64ef66ac233ab1b7e2345f
vagrant <vagrant@vagrant.vm>
2019-11-04 20:08:55 +0000

    Working home page redirect to todays date and login form

---
+++ model.py
@@ -0,0 +1,29 @@
+# Models and database functions 
+from flask_sqlalchemy import SQLAlchemy
+
+# This is the connection to the PostgreSQL database; we're getting this through
+# the Flask-SQLAlchemy helper library. On this, we can find the `session`
+# object, where we do most of our interactions (like committing, etc.)
+
+db = SQLAlchemy()
+
+
+def connect_to_db(app):
+    """Connect the database to our Flask app."""
+
+    # Configure to use our PostgreSQL database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///demo'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_ECHO'] = True
+    db.app = app
+    db.init_app(app)
+
+
+if __name__ == "__main__":
+    # As a convenience, if we run this module interactively, it will leave
+    # you in a state of being able to work with the database directly.
+
+    from server import app
+
+    connect_to_db(app)
+    print("Connected to DB.")
--- server.py
+++ server.py
@@ -1,1 +1,67 @@
-print('Hello world!')
+from jinja2 import StrictUndefined
+
+from flask import Flask, render_template, request, flash, redirect, session
+from flask_debugtoolbar import DebugToolbarExtension
+from datetime import datetime, date, timedelta
+
+from model import connect_to_db
+
+
+app = Flask(__name__)
+
+# Required to use Flask sessions and the debug toolbar
+app.secret_key = "super-secret"
+
+# raise an error in Jinja2 when using an undefined variable
+app.jinja_env.undefined = StrictUndefined
+
+# current date needed to default homepage to today's date
+current_date = (date.today()).strftime('%Y-%m-%d')
+
+@app.route('/')
+def index():
+    """Defaults user view to today's log entries"""
+
+    return redirect(f"/{current_date}")
+
+
+@app.route(f"/<selected_date>")
+def daily_view(selected_date=current_date):
+    """Daily view of foods eaten"""
+
+    day_value = datetime.strptime(selected_date, '%Y-%m-%d')
+
+    day_forward = (day_value + timedelta(days=1)).strftime('%Y-%m-%d')
+    day_backward = (day_value + timedelta(days=-1)).strftime('%Y-%m-%d')
+
+    return render_template(
+                        'daily_view.html', 
+                        selected_date=selected_date,
+                        day_forward=day_forward,
+                        day_backward=day_backward,
+                        current_date=current_date,
+                        )
+
+@app.route("/login", methods=['GET'])
+def login_form():
+    """Show login form"""
+
+    return render_template('login.html')
+
+@app.route("/login", methods=['POST'])
+def process_login():
+    """Log a user in if the user is in the database and provides correct password"""
+
+    pass
+
+if __name__ == "__main__":
+
+    # Remove debug for demo
+    app.debug = True
+
+    connect_to_db(app)
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+    app.run(host="0.0.0.0")
