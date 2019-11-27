a915f4fafa48f2d2417d68e0870b66cd5accb5a3
Kelly Hofmann <kellymhofmann@gmail.com>
2019-11-27 10:24:22 -0800

    Wrote a test for logging in a user

--- tests.py
+++ tests.py
@@ -1,51 +1,18 @@
 import unittest
 from server import app
-from model import (db, connect_to_db, User, Food, FoodIngredient, Ingredient, 
-    Symptom, SymptomLog, FoodLog, UserSymptomIngredientLink, Meal, example_data,
-    init_app)
+from model import *
 from flask import session
 
-class FlaskTestsDatabase(unittest.TestCase):
-    """Flask tests that use the database."""
-
-    def setUp(self):
-        """Stuff to do before every test."""
-
-        # Get the Flask test client
-        self.client = app.test_client()
-        app.config['TESTING'] = True
-
-        # Connect to test database
-        connect_to_db(app, db_uri="postgresql:///testdb")
-
-        # Create tables and add sample data
-        db.create_all()
-        example_data()
-
-    def tearDown(self):
-        """Do at end of every test."""
-
-        db.session.remove()
-        db.drop_all()
-        db.engine.dispose()
-
-    # def test_ingredients_list(self):
-    #     """Test the ingredients list"""
-
-    #     result = self.client.get("/api/symptoms")
-    #     print(result.data)
-    #     self.assertIn(b"heartburn", result.data)
-        # TO DO check to see if it's json
-
-class FlaskTestsLoggedIn(unittest.TestCase):
+class FlaskTestsMain(unittest.TestCase):
     """Flask tests with user logged in to session."""
 
     def setUp(self):
         """Stuff to do before every test."""
 
+        self.client = app.test_client()
         app.config['TESTING'] = True
         app.config['SECRET_KEY'] = 'most-secret'
-        self.client = app.test_client()
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
         # Connect to test database
         connect_to_db(app, db_uri="postgresql:///testdb")
@@ -72,6 +39,46 @@ class FlaskTestsLoggedIn(unittest.TestCase):
 
         result = self.client.get("/2019-11-26")
         self.assertIn(b"Your food diary for", result.data)
+
+
+class FlaskTestsLogInLogOut(unittest.TestCase):
+    """Flask tests with DB but user not logged in (yet)"""
+
+    def setUp(self):
+        """Stuff to do before every test."""
+
+        # Set up the Flask test client
+        app.config['TESTING'] = True
+        app.config['SECRET_KEY'] = 'most-secret'
+        self.client = app.test_client()
+
+        # Connect to test database
+        connect_to_db(app, db_uri="postgresql:///testdb")
+
+        # Create tables and add sample data
+        db.create_all()
+        example_data()
+
+    def tearDown(self):
+        """Do at end of every test."""
+
+        db.session.remove()
+        db.drop_all()
+        db.engine.dispose()
+
+    def test_process_login(self):
+        """Test if an existing user is able to log in"""
+
+        result = self.client.post('/login',
+                                  data={
+                                    'email': 'john@doe.com', 
+                                    'password': 'fluffy89',
+                                  },
+                                  follow_redirects=True)
+
+        self.assertEqual(result.status_code, 200)
+        self.assertIn(b'<p>Here would be a daily view of:</p>', result.data)
+
 
 
 if __name__ == "__main__":
